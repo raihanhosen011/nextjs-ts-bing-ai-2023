dff3f5dd56a7b4b8969e9bcff45074268f936a6e
raihanhosen011 <raihanhosen011@gmail.com>
2023-03-19 16:19:34 +0600

    Fix

--- src/modules/Chat/helpers/ToMessage.tsx
+++ src/modules/Chat/helpers/ToMessage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+//
+import { ConversationProps } from '@config/types'
 
-const ToMessage = () => {
+
+const ToMessage : React.FC<ConversationProps> = ({ response }) => {
   return (
-    <div className='float-right bg-gradient-to-r from-blue-500 to-blue-700 p-3 rounded-xl'>
-        <p className='text-md text-white'>I want to buid same to same to you. so how can i do that
-</p>
+    <div className='w-fit ml-auto my-3 bg-gradient-to-r from-blue-500 to-blue-700 p-3 rounded-xl'>
+      <p className='text-md text-white'> {response} </p>
     </div>
   )
 }
