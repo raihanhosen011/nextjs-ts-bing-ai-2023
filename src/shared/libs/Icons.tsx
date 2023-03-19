caa19a86152fb7e6685a7ed5694fb109e871dc62
raihanhosen011 <raihanhosen011@gmail.com>
2023-03-19 16:20:12 +0600

    Fix

--- src/shared/libs/Icons.tsx
+++ src/shared/libs/Icons.tsx
@@ -1,8 +1,12 @@
 // import icons 
 import { SiDiscord } from 'react-icons/si'
+import { AiOutlineMessage } from "react-icons/ai";
+import { BsSendFill } from "react-icons/bs";
 
 
 // export 
 export const icons : any = {
-    discord : <SiDiscord />
+    discord : <SiDiscord />,
+    message : <AiOutlineMessage />,
+    send : <BsSendFill />
 }
