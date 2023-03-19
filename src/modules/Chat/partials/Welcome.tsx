994509ba18369a924c77637abffc7305d39d4bfd
raihanhosen011 <raihanhosen011@gmail.com>
2023-03-19 22:20:51 +0600

    FIx

--- src/modules/Chat/partials/Welcome.tsx
+++ src/modules/Chat/partials/Welcome.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import React from "react";
-import mainLogo from "../../../../public/img/bing_logo.png";
+//
+import mainLogo from "../../../../public/img/brand.png";
 
 const Welcome = () => {
   return (
     <div className="flex h-[500px] flex-col gap-3 text-center justify-center">
-      <div>
-        <Image src={mainLogo} className="object-cover" width={30} height={40} alt="chat logo" />
+      <div className="w-[100px] md:w-[150px] mx-auto md:mb-7" >
+        <Image src={mainLogo} className="object-cover" width={1} height={1} layout='responsive' alt="chat logo" />
       </div>
-      <h2 className="text-[36px] font-[600]">Welcome to the new Bing</h2>
-      <p className="text-[18px] font-[400]">
+
+      <h2 className="text-[18px] md:text-[36px] font-[600] mb-[-7px] md:mb-[-12px]">Powerful search engine for your enterprise.</h2>
+      <p className="text-[15px] md:text-[18px] font-[400]">
         Your AI-powered copilot for the web
       </p>
     </div>
