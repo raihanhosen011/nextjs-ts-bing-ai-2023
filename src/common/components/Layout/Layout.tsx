6c852829ad7a7c60a988a65a0ae7e779b9736cee
raihanhosen011 <raihanhosen011@gmail.com>
2023-03-19 22:20:21 +0600

    FIx

--- src/common/components/Layout/Layout.tsx
+++
@@ -1,8 +0,0 @@
-import Footer from "@widgets/Footer";
-import Header from "@widgets/Header";
-import React from "react";
-// default layout configuration
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
-};
-export default Layout;
