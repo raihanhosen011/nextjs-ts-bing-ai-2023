921423cf7e9e2d7a0d347e4193d7af56815112f1
raihanhosen011 <raihanhosen011@gmail.com>
2023-03-19 22:20:58 +0600

    FIx

--- src/shared/libs/Icons.tsx
+++ src/shared/libs/Icons.tsx
@@ -2,11 +2,13 @@
 import { SiDiscord } from 'react-icons/si'
 import { AiOutlineMessage } from "react-icons/ai";
 import { BsSendFill } from "react-icons/bs";
+import { MdRestartAlt } from "react-icons/md";
 
 
 // export 
 export const icons : any = {
     discord : <SiDiscord />,
     message : <AiOutlineMessage />,
-    send : <BsSendFill />
+    send : <BsSendFill />,
+    reset : <MdRestartAlt />
 }
