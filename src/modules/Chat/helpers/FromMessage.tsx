baa600c9460d45d4c9390d2c4f175b3aba5185b0
raihanhosen011 <raihanhosen011@gmail.com>
2023-03-19 16:19:20 +0600

    Fix

--- src/modules/Chat/helpers/FromMessage.tsx
+++ src/modules/Chat/helpers/FromMessage.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import LearnMoreWithTag from './LearnMoreWithTag'
-import MessageBox from './MessageBox'
+//
+import { ConversationProps } from '@config/types'
 
-const FromMessage = () => {
+
+const FromMessage:React.FC<ConversationProps> = ({ response, handleButtonSubmit }) => {
   return (
     <div className='bg-[#ffffff] rounded-lg shadow w-full mt-3'>
-      <MessageBox />
-      <LearnMoreWithTag />
+      
+      {response?.custom && <div className='p-3'>
+         {response.custom.answer}
+      </div>}
+
+      {response?.text && <div className='p-3'>
+         {response.text}
+      </div>}
+
+      {response?.buttons && <div className="border-t p-3 flex-wrap flex items-center gap-2">
+        <ul className="flex items-center gap-3 flex-wrap">
+          {response.buttons.map((e:any, i:number) => (
+              <li 
+                className="text-sm bg-blue-200 text-blue-800 cursor-pointer px-2 py-1 rounded-md"
+                onClick={() => handleButtonSubmit(e)}
+              >
+                {e.title}
+              </li>
+          ))}
+        </ul>
+      </div>}
     </div>
   )
 }
