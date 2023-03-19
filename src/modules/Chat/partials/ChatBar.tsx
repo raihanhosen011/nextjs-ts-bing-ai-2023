eb47c21ed4aac7034d987786c7b6d4e828e924ec
raihanhosen011 <raihanhosen011@gmail.com>
2023-03-19 22:20:48 +0600

    FIx

--- src/modules/Chat/partials/ChatBar.tsx
+++ src/modules/Chat/partials/ChatBar.tsx
@@ -6,28 +6,32 @@ import { ChatBarProps } from "@config/types";
 
 const ChatBar : React.FC<ChatBarProps> = ({ handleSubmit, setInputData, inputData }) => {
   return (
-    <div className="fixed bottom-0 flex justify-center items-center text-center w-full left-0 mx-3 global pb-8 pt-3">
-      <form 
-        className="w-full bg-white sm:w-[80%] relative rounded-full flex items-center resize-none gap-3 border-none outline-none transition-all duration-500 ease-in-out px-5 py-4 shadow scrolls"
-        onSubmit={handleSubmit}
-      >
+    <div className="fixed bottom-0 flex justify-center items-center text-center w-full left-0 global pb-8 pt-3">
+      <div className="container" >
 
-        <span className="text-2xl top-3 text-gray-700" >{icons.message}</span>
-
-        <input
-          className="w-full h-full bg-transparent outline-none resize-none border-none text-gray-700"
-          placeholder="Ask me anything..."
-          onChange={(e:ChangeEvent<HTMLInputElement>) => setInputData({ title : e.target.value, payload : e.target.value })}
-          value={inputData.title}
-        />
-
-        <button
-          className="text-2xl top-3 text-gray-700"
+        <form 
+          className="w-full bg-white relative rounded-full flex items-center resize-none gap-3 border-none outline-none transition-all duration-500 ease-in-out px-5 py-4 shadow scrolls"
+          onSubmit={handleSubmit}
         >
-          {icons.send}
-        </button>
 
-      </form>
+          <span className="text-2xl top-3 text-gray-700" >{icons.message}</span>
+
+          <input
+            className="w-full h-full bg-transparent outline-none resize-none border-none text-gray-700"
+            placeholder="Ask me anything..."
+            onChange={(e:ChangeEvent<HTMLInputElement>) => setInputData({ title : e.target.value, payload : e.target.value })}
+            value={inputData.title}
+          />
+
+          {inputData.title.length > 0 && <button
+            className="text-2xl top-3 text-gray-700"
+          >
+            {icons.send}
+          </button>}
+
+        </form>
+
+      </div>
     </div>
   );
 };
