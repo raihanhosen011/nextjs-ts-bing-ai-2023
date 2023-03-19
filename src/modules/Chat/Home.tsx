a147b370048ea16848de9368de7051ed6d143c25
raihanhosen011 <raihanhosen011@gmail.com>
2023-03-19 16:19:15 +0600

    Fix

--- src/modules/Chat/Home.tsx
+++ src/modules/Chat/Home.tsx
@@ -1,29 +1,89 @@
-import React from "react";
+import Image from "next/image";
+import React, { FormEvent, useRef, useState } from "react";
+//
+import { usePost } from "@hooks/useFetch";
 import FromMessage from "./helpers/FromMessage";
-import Generate from "./helpers/Generate";
-import Result from "./helpers/Result";
 import ToMessage from "./helpers/ToMessage";
 import ChatBar from "./partials/ChatBar";
 import Welcome from "./partials/Welcome";
+import { ConversationProps } from "@config/types";
+
 
 const Home = () => {
+
+  // states
+  const [inputData, setInputData] = useState({ title : '', payload : '' })
+  const [conversation, setConversation] = useState<Array<ConversationProps>>([])
+  const messagesRef = useRef<any>(null);
+
+  // hooks
+  const { mutate, isSuccess, isError, error, isLoading, data } = usePost('webhooks/rest/webhook')
+
+  // submit
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    setConversation([...conversation, { sender: "user", response: inputData.title }]);
+    setInputData({ title : '', payload : '' })
+    await mutate({ message : inputData.payload }) 
+  }
+
+  // button submit
+  const handleButtonSubmit = async (payload: any) => {
+    setConversation([...conversation, { sender: "user", response: payload.title }]);
+    await mutate({ message : payload.payload })
+  }
+
+  const scrollToBottom = () => {
+    messagesRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // effects
+  React.useEffect(() => {
+    if (isSuccess) {
+      setConversation([...conversation, { sender: "bot", response: data.data }]);
+      localStorage.setItem("messages", JSON.stringify([...conversation, { sender: "bot", response: data.data }]));
+    }else if(isError){
+      setConversation([...conversation, { sender: "bot", response: [{ text : "Unfortunately, I'm having some problem 😅. I would appreciate it if you could try again later" }] }]);
+      localStorage.setItem("messages", JSON.stringify([...conversation, { sender: "bot", response: [{ text : "Unfortunately, I'm having some problem 😅. I would appreciate it if you could try again later" }] }]));
+    }
+  }, [isSuccess, isError])
+  
+  React.useEffect(() => {
+    const messages = JSON.parse(localStorage.getItem("messages") || "[]");
+    setConversation(messages);
+  }, []);
+
+  React.useEffect(() => {
+    scrollToBottom()
+  }, [conversation]);
+
+
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto" >
       <Welcome />
 
-      <div className="flex gap-8 flex-col-reverse pb-[10rem]">
-        <div className=" lg:w-2/3 w-full">
-          <Result />
-          <Generate />
-          <FromMessage />
-        </div>
-        <div className="w-full order-1">
-          <ToMessage />
-        </div>
-      </div>
-     
+      <div className="gap-8 pb-[10rem]"  ref={messagesRef} >
+        {conversation.map((message, index) => (
+          <div className={`${message.sender === 'user' ? 'user-msg lg:w-1/3 w-full ml-auto' : 'bot-msg w-full lg:w-2/3 order-1'} w-full`} key={index}>
+            {message.sender === 'user' ? (
+              <ToMessage response={message.response} />
+            ) : (
+              <>
+                <FromMessage response={message.response[0]} handleButtonSubmit={handleButtonSubmit} />
+              </>
+            )}
+          </div>
+        ))}
 
-      <ChatBar />
+        {isLoading && <div className=" w-full lg:w-2/3 order-1" >
+          <div className="h-[35px] w-[70px] bg-white shadow rounded-[7px] overflow-hidden relative" > 
+            <Image src='/img/gif/loading.gif' layout='fill' objectFit='contain' />
+          </div>
+        </div>}
+      </div>
+
+      <ChatBar handleSubmit={handleSubmit} setInputData={setInputData} inputData={inputData} />
     </div>
   );
 };
