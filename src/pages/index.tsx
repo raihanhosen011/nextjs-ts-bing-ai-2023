0a86926582376a3472435fa961b02ae40ca2efad
raihanhosen011 <raihanhosen011@gmail.com>
2023-03-19 22:20:53 +0600

    FIx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
-import { v4 as uuidv4 } from "uuid";
 import type { NextPage } from "next";
 //
-import HomePage from "@modules/Chat";
+import ChatPage from "@modules/Chat";
 
 const Home: NextPage = () => {
-
-    // sender _id
-    const sender_id = uuidv4().slice(0, 8);
-
     return (
         <>
             <div className="__home">
-                 <HomePage sender_id={sender_id} />
+                 <ChatPage />
             </div>
         </>
     );
