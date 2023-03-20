c10e537fb8e987788169457ba7f643ef744a32a6
raihanhosen011 <raihanhosen011@gmail.com>
2023-03-20 22:22:35 +0600

    FIX

--- src/modules/Chat/helpers/FromMessage.tsx
+++ src/modules/Chat/helpers/FromMessage.tsx
@@ -5,6 +5,7 @@ import Texthighlighter from './Texthighlighter'
 
 
 const FromMessage:React.FC<ConversationProps> = ({ response, handleButtonSubmit }) => {
+
   return (
     <div className='bg-[#ffffff] rounded-lg shadow w-full mt-3'>
 
@@ -15,6 +16,25 @@ const FromMessage:React.FC<ConversationProps> = ({ response, handleButtonSubmit 
       {response?.text && <div className='p-3'>
          {response.text}
       </div>}
+
+      {response?.custom?.urls && 
+        <div className="border-t p-3 flex items-center gap-2">
+          <ul className="flex items-center gap-3 flex-wrap">
+            <h1 className='font-[500]' > Learn More : </h1>
+
+            {response?.custom?.urls.map((e:any, i:number) => (
+              <a href={e.url} target="_blank" >
+                <li 
+                  key={i}
+                  className="text-sm bg-[#59ffa042] text-[#197f46] font-[500] cursor-pointer px-2 py-1 rounded-md"
+                >
+                  {e?.title?.split('.')[1]}
+                </li>
+              </a>
+            ))}
+          </ul>
+        </div>
+      }
 
       {response?.buttons && <div className="border-t p-3 flex-wrap flex items-center gap-2">
         <ul className="flex items-center gap-3 flex-wrap">
